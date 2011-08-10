dcc6005cd04fd23dd000624c9ed33c0ab907bbfe
Vanja Komadinovic <vanjakom@gmail.com>
2011-08-11 00:27:12 +0200

    client and server side SSL example coded in NodeJS

---
+++ client_server_ssl_nodejs/client.js
@@ -0,0 +1,27 @@
+var https = require('https');
+var fs = require("fs");
+
+var options = {
+	host: 'localhost',
+	port: 8000,
+	path: '/test',
+	method: 'GET',
+	key: fs.readFileSync("keys/userB.key"),
+	cert: fs.readFileSync("certs/userB.crt"),
+	ca: fs.readFileSync("ca/ca.crt")
+};
+
+var req = https.request(options, function(res) {
+	console.log("statusCode: ", res.statusCode);
+	console.log("headers: ", res.headers);
+	
+	res.on('data', function(d) {
+    	process.stdout.write(d);
+  	});
+});
+
+req.end();
+
+req.on('error', function(e) {
+	console.error(e);
+});
---
+++ client_server_ssl_nodejs/server.js
@@ -0,0 +1,19 @@
+var sys = require("sys");
+var fs = require("fs");
+var https = require("https");
+
+var options = {
+  key: fs.readFileSync("keys/server.key"),
+  cert: fs.readFileSync("certs/server.crt"),
+  ca: fs.readFileSync("ca/ca.crt"),
+  requestCert: true,
+  rejectUnauthorized: true 
+};
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  sys.puts("request from: " + req.connection.getPeerCertificate().subject.CN);
+  res.end("Hello World, " + req.connection.getPeerCertificate().subject.CN + "\n");
+}).listen(8000);
+
+sys.puts("server started");
